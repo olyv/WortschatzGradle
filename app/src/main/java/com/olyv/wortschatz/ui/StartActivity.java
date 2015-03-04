428b4134b9157cef2688cf9af1df8d6f6d27f494
olyv <a.n.lyvin@gmail.com>
2015-03-04 15:00:44 +0200

    added repeating notification (AlarmManager)

--- app/src/main/java/com/olyv/wortschatz/ui/StartActivity.java
+++ app/src/main/java/com/olyv/wortschatz/ui/StartActivity.java
@@ -1,12 +1,20 @@
 package com.olyv.wortschatz.ui;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
@@ -14,11 +22,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
 import com.olyv.wortschatz.lesson.DatabaseHelper;
 import com.olyv.wortschatz.lesson.LessonItemsHelper;
 import com.olyv.wortschatz.lesson.items.LessonItemI;
-import com.olyv.wortschatz.ui.lesson.LessonActivity;
 import com.olyv.wortschatz.ui.editor.AddNewItemActivity;
+import com.olyv.wortschatz.ui.lesson.LessonActivity;
 import com.olyv.wortschatz.ui.manager.LessonItemsManagerActivity;
 import com.olyv.wortschatz.ui.settings.NumberPickerPreference;
 import com.olyv.wortschatz.ui.settings.SettingsActivity;
@@ -28,9 +37,11 @@ import java.util.Collections;
 
 public class StartActivity extends Activity
 {
-    public static final String LOG_TAG = "StartActivityLog";
     public static final String PREPARED_LESSON = "preparedLessonItems";
     public static final String OPEN_MANAGER = "openItemsManager";
+
+    private static final int NOTIFICATION_ID = 1;
+    private static final String LOG_TAG = "StartActivityLog";
 
     private DatabaseHelper databaseHelper = null;
 
@@ -39,6 +50,10 @@ public class StartActivity extends Activity
     private Button addNewItemButton;
 
     private LinearLayout loadingIndicator;
+    private AlarmManager alarmManager;
+    private Intent notificationReceiverIntent;
+    private PendingIntent notificationReceiverPendingIntent;
+    private static boolean running;     //flag to identify if app is running and ignore sent notification
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -83,18 +98,75 @@ public class StartActivity extends Activity
             }
         });
         Log.i(LOG_TAG, "initialized layout and UI elements");
+
+        running = true;
+    }
+
+    public static class AlarmReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            if (!running)
+            {
+                // The Intent to be used when the user clicks on the Notification View
+                Intent notificationIntent = new Intent(context, StartActivity.class);
+
+                // The PendingIntent that wraps the underlying Intent
+                PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                long[] mVibratePattern = { 0, 200, 200, 300 };
+                String uriPath = "android.resource://com.olyv.wortschatz.ui/" + R.raw.notification;
+
+                Uri soundURI = Uri.parse(uriPath);
+
+                // Build the Notification
+                Notification.Builder notificationBuilder = new Notification.Builder(
+                        context).setTicker("I am Wortschatz")
+                        .setSmallIcon(android.R.drawable.stat_sys_warning)
+                        .setAutoCancel(true).setContentTitle("contentTitle")
+                        .setContentText("contentText").setContentIntent(contentIntent)
+                        .setSound(soundURI).setVibrate(mVibratePattern);
+
+                // Get the NotificationManager
+                NotificationManager notificationManager = (NotificationManager) context
+                        .getSystemService(Context.NOTIFICATION_SERVICE);
+
+                // Pass the Notification to the NotificationManager:
+                notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+
+                Log.e(LOG_TAG, "========================= notification received");
+            }
+        }
     }
 
     @Override
-    protected void onDestroy()
+    protected void onStop()
     {
-        super.onDestroy();
+        super.onStop();
         DatabaseHelper.releaseDatabaseHelper(databaseHelper);
         Log.i(LOG_TAG, "released DatabaseHelper");
+
+        //sending notifications logic
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        boolean notificationsEnabled = prefs.getBoolean(getString(R.string.preference_notify_me_key),false);
+        if (notificationsEnabled)
+        {
+            alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            notificationReceiverIntent = new Intent(getApplicationContext(), AlarmReceiver.class);
+            notificationReceiverPendingIntent = PendingIntent.getBroadcast(StartActivity.this, 0, notificationReceiverIntent, 0);
+            alarmManager.setInexactRepeating(
+                    AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime() + 15 * 100L,
+                    20000,
+                    notificationReceiverPendingIntent);
+        }
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         loadingIndicator.setVisibility(View.GONE);
     }
