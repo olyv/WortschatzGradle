de37b5ada1ca74a4b898fb973b8b95d52261b371
olyv <a.n.lyvin@gmail.com>
2015-04-17 14:48:33 +0300

    added remove item test

--- app/src/androidTest/java/com/olyv/wortschatz/test/integration/manager/ItemsManagerActivityTest.java
+++ app/src/androidTest/java/com/olyv/wortschatz/test/integration/manager/ItemsManagerActivityTest.java
@@ -1,25 +1,17 @@
 package com.olyv.wortschatz.test.integration.manager;
 
-import android.app.AlertDialog;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import com.olyv.wortschatz.lesson.DatabaseHelper;
 import com.olyv.wortschatz.lesson.LessonItemsHelper;
-import com.olyv.wortschatz.lesson.items.LessonItemI;
-import com.olyv.wortschatz.lesson.items.Noun;
 import com.olyv.wortschatz.lesson.items.Verb;
-import com.olyv.wortschatz.test.integration.addword.BaseAddWordTest;
 import com.olyv.wortschatz.ui.R;
 import com.olyv.wortschatz.ui.StartActivity;
-import com.olyv.wortschatz.ui.editor.AddNewItem;
 import com.olyv.wortschatz.ui.manager.LessonItemsManagerActivity;
 import com.robotium.solo.Solo;
 
 import junit.framework.Assert;
-
-import java.util.ArrayList;
 
 public class ItemsManagerActivityTest extends ActivityInstrumentationTestCase2<StartActivity>
 {
